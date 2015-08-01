75290dec4bab36acd0a1ea51a5fe8cc030c02f85
Bumsoo Kim <bskim45@gmail.com>
2015-08-01 18:21:15 +0900

    added progress report when download installer

--- CCleanerAutoUpdate.py
+++ CCleanerAutoUpdate.py
@@ -100,9 +100,9 @@ def CheckUpdate(config):
 
     print 'Begin download...'
     obj = urllib2.urlopen(url)
+
     with open(filename, 'wb') as f:
-        f.write(obj.read())
-        obj.close()
+        chunk_read(f, obj, report_hook=chunk_report)
 
     print 'Download complete!'
 
@@ -117,6 +117,37 @@ def CheckUpdate(config):
         DeleteInstaller()
 
     return 0
+
+
+def chunk_report(bytes_so_far, chunk_size, total_size):
+    percent = float(bytes_so_far) / total_size
+    percent = round(percent * 100, 2)
+    sys.stdout.write("Downloaded %d of %d bytes (%0.2f%%)\r" %
+                     (bytes_so_far, total_size, percent))
+
+    if bytes_so_far >= total_size:
+        sys.stdout.write('\n')
+
+
+def chunk_read(f, response, chunk_size=8192, report_hook=None):
+    total_size = response.info().getheader('Content-Length').strip()
+    total_size = int(total_size)
+    bytes_so_far = 0
+
+    while 1:
+        chunk = response.read(chunk_size)
+        f.write(chunk)
+        bytes_so_far += len(chunk)
+
+        if not chunk:
+            break
+
+        if report_hook:
+            report_hook(bytes_so_far, chunk_size, total_size)
+
+    f.close()
+
+    return bytes_so_far
 
 
 def ParseConfig(configfile):
